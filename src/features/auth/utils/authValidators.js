aa5d86173f6a16ad8496de12893f453099a50b1f
RocioVera <rocioveradaniela@gmail.com>
2026-06-24 13:19:59 -0300

    nuevos cambios en const prueba

--- src/features/auth/utils/authValidators.js
+++ src/features/auth/utils/authValidators.js
@@ -8,7 +8,8 @@ export const PASSWORD_REGEXES = {
 const NAME_REGEX = /^[A-Za-zÁéíóúáéíóúÑñ\s]+$/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-const prueba ="";
+const prueba ="se agrega prueba";
+
 
 export const checkPasswordStrength = (password) => {
   return {
